7bcc88c79517336a3c28e1dd28a6fdec7436147a
David Li <zdavidli@gmail.com>
2017-12-02 20:29:26 -0500

    wtf

--- PeopleArtDataset.py
+++ PeopleArtDataset.py
@@ -1,40 +1,30 @@
-import numpy as np
-import os.path
-import torchvision.datasets as dset
-from torch.utils.data import DataLoader, Dataset
-import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
 from PIL import Image
-
-im_folder = dset.ImageFolder(root="./PeopleArt50-master/JPEGImages")
-
-train = np.loadtxt("./PeopleArt50-master/Annotations/person_train.txt", dtype="str", ndmin=2)
-test = np.loadtxt("./PeopleArt50-master/Annotations/person_test.txt", dtype="str", ndmin=2)
-
-def peopleart_loader(path):
-    with open(os.path.join("./PeopleArt50-master/JPEGImages", path), 'rb') as f:
-        with Image.open(f) as img:
-            return img.convert('RGB')
+import os
 
 class PeopleArtDataset(Dataset):
-    def __init__(self, train, transform=None):
+
+    
+    def __init__(self, root="../PeopleArt-master/JPEGImages", train=True, transform=None):
+        self.root = root
         self.transform = transform
         self.train = train
-
-    def __getitem__(self, index):
         if self.train:
-            im = peopleart_loader(train[index][0])
-            label = int(train[index][1])
-
+            self.files = np.loadtxt("../PeopleArt-master/Annotations/person_train.txt", dtype='str', ndmin=2)
         else:
-            im = peopleart_loader(test[index][0])
-            label = int(test[index][1])
-
-        if self.transform is not None:
-            im = self.transofmr(im)
-
-        return im, label
-
+            self.files = np.loadtxt("../PeopleArt-master/Annotations/person_test.txt", dtype='str', ndmin=2)
+            
+#         print(self.files)
+    
     def __len__(self):
-        if self.train:
-            return train.shape[0]
-        return test.shape[0]
+        return self.files.shape[0]
+
+    def __getitem__(self, idx):
+        im_name = self.files[idx]
+        im = Image.open(os.path.join(self.root, im_name[0]))
+        label = int(int(im_name[1]) > 0)
+        
+        if self.transform:
+            im = self.transform(im)
+        print(im_name)    
+        return im, label
--- get_data.sh
+++ get_data.sh
@@ -1,2 +1,4 @@
-git clone https://github.com/BathVisArtData/PeopleArt.git
-git clone https://github.com/BathVisArtData/PhotoArt50.git
+wget -qO- -O tmp.zip https://github.com/BathVisArtData/PeopleArt/archive/master.zip && unzip tmp.zip && rm tmp.zip
+
+wget -qO- -O tmp.zip https://github.com/BathVisArtData/PhotoArt50/archive/master.zip && unzip tmp.zip && rm tmp.zip
+
