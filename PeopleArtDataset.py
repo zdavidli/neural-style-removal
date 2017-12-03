477924b8eda802d2e996b509193c1b4d849ac517
David Li <zdavidli@gmail.com>
2017-12-03 19:39:58 +0000

    added photo dataset

--- PeopleArtDataset.py
+++ PeopleArtDataset.py
@@ -3,7 +3,6 @@ from PIL import Image
 import os
 
 class PeopleArtDataset(Dataset):
-
     
     def __init__(self, root="../PeopleArt-master/JPEGImages", train=True, transform=None):
         self.root = root
@@ -26,5 +25,22 @@ class PeopleArtDataset(Dataset):
         
         if self.transform:
             im = self.transform(im)
-        print(im_name)    
+#         print(im_name)    
         return im, label
+    
+class PeopleArtPhotoDataset(Dataset):
+    #Gets photos, no labels
+    def __init__(self, root="../PeopleArt-master/JPEGImages/photo", transform=None):
+        self.root = root
+        self.transform = transform
+        self.files =  [files for r,d,files in os.walk(self.root)][0]
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        im_name = self.files[idx]
+        im = Image.open(os.path.join(self.root, im_name))
+        if self.transform:
+            im = self.transform(im)
+        return im
